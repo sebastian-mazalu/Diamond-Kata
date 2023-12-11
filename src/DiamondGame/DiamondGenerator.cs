8b383b04b3c0e6f9858112d1a24821928d46eb97
Sebastian Mazalu <sebastian.mazalu@gmail.com>
2023-12-11 16:41:56 +0200

    Added unit tests foro the DiamondGenerator

--- src/DiamondGame/DiamondGenerator.cs
+++ src/DiamondGame/DiamondGenerator.cs
@@ -1,4 +1,6 @@
 ﻿using DiamondGame.Contracts;
+using System.Runtime.InteropServices;
+using System.Text;
 
 namespace DiamondGame;
 
@@ -6,12 +8,101 @@ public class DiamondGenerator : IDiamondGenerator
 {
 	public string GenerateDiamond(char diamondLetter, bool displayWhiteSpaces = false)
 	{
-		return @"
-_ _ A _ _
-_ B _ B _
-C _ _ _ C
-_ B _ B _
-_ _ A _ _
-";
+		if (diamondLetter == 'A') return GetDiamondA(displayWhiteSpaces);
+
+		if (diamondLetter == 'B') return GetDiamondB(displayWhiteSpaces);
+
+		return GetDiamond(diamondLetter, displayWhiteSpaces);
+	}
+
+	private static string GetDiamondA(bool displayWhiteSpaces) => "A";
+
+	private static string GetDiamondB(bool displayWhiteSpaces) =>
+		displayWhiteSpaces ?
+			$"_ A _ {Environment.NewLine}B _ B{Environment.NewLine}_ A _ " :
+			$" A {Environment.NewLine}B B{Environment.NewLine} A ";
+
+	private static string GetDiamond(char diamondLetter, bool displayWhiteSpaces)
+	{
+		var diamondBuilder = new StringBuilder();
+
+		diamondBuilder = BuildTheFirstHalfOfTheDiamond(diamondLetter, displayWhiteSpaces, diamondBuilder);
+
+		diamondBuilder = BuildTheMiddleLineOfTheDiamond(diamondLetter, displayWhiteSpaces, diamondBuilder);
+
+		diamondBuilder = BuildTheSecondHalfOfTheDiamond(diamondLetter, displayWhiteSpaces, diamondBuilder);
+
+		return diamondBuilder.ToString().TrimEnd(Environment.NewLine.ToCharArray());
+	}
+
+	private static StringBuilder BuildTheFirstHalfOfTheDiamond(char diamondLetter, bool displayWhiteSpaces, StringBuilder diamondBuilder)
+	{
+		var maxDistanceBetweenLetters = diamondLetter - 'A';
+		var currentLetter = 'A';
+		var i = maxDistanceBetweenLetters;
+		var separator = displayWhiteSpaces ? "_ " : " ";
+
+		for (; i > 0; i--, currentLetter++)
+		{
+			var spacesToFirstLetter = new StringBuilder().Insert(0, separator, i).ToString();
+			var currentLetterTermination = displayWhiteSpaces ? " " : "";
+
+			if (currentLetter == 'A')
+			{
+				diamondBuilder.AppendLine($"{spacesToFirstLetter}A{currentLetterTermination}{spacesToFirstLetter}");
+			}
+			else
+			{
+				var spacesBetweenLetters = new StringBuilder().Insert(0, separator, (maxDistanceBetweenLetters - i) * 2 - 1).ToString();
+				var currentLine = string.Format("{0}{1}{2}{3}{1}{2}{0}", spacesToFirstLetter, currentLetter, currentLetterTermination, spacesBetweenLetters);
+
+				diamondBuilder.AppendLine(currentLine);
+			}
+		}
+
+		return diamondBuilder;
+	}
+
+	private static StringBuilder BuildTheMiddleLineOfTheDiamond(char diamondLetter, bool displayWhiteSpaces, StringBuilder diamondBuilder)
+	{
+		var maxDistanceBetweenLetters = diamondLetter - 'A';
+		var separator = displayWhiteSpaces ? "_ " : " ";
+		var currentLetterTermination = displayWhiteSpaces ? " " : "";
+
+		var spacesBetweenLetters = new StringBuilder().Insert(0, separator, maxDistanceBetweenLetters * 2 - 1).ToString();
+		var currentLine = string.Format("{0}{1}{2}{0}", diamondLetter, currentLetterTermination, spacesBetweenLetters);
+
+		diamondBuilder.AppendLine(currentLine);
+
+		return diamondBuilder;
+	}
+
+	private static StringBuilder BuildTheSecondHalfOfTheDiamond(char diamondLetter, bool displayWhiteSpaces, StringBuilder diamondBuilder)
+	{
+		var maxDistanceBetweenLetters = diamondLetter - 'A';
+		var currentLetter = diamondLetter;
+		currentLetter--;
+		var i = 1;
+		var separator = displayWhiteSpaces ? "_ " : " ";
+
+		for (; i <= maxDistanceBetweenLetters; i++, currentLetter--)
+		{
+			var spacesToFirstLetter = new StringBuilder().Insert(0, separator, i).ToString();
+			var currentLetterTermination = displayWhiteSpaces ? " " : "";
+
+			if (currentLetter == 'A')
+			{
+				diamondBuilder.AppendLine($"{spacesToFirstLetter}A{currentLetterTermination}{spacesToFirstLetter}");
+			}
+			else
+			{
+				var spacesBetweenLetters = new StringBuilder().Insert(0, separator, (maxDistanceBetweenLetters - i) * 2 - 1).ToString();
+				var currentLine = string.Format("{0}{1}{2}{3}{1}{2}{0}", spacesToFirstLetter, currentLetter, currentLetterTermination, spacesBetweenLetters);
+
+				diamondBuilder.AppendLine(currentLine);
+			}
+		}
+
+		return diamondBuilder;
 	}
 }
---
+++ src/DiamondGameTests/DiamondGeneratorTests.cs
@@ -0,0 +1,118 @@
+﻿using DiamondGame;
+using FluentAssertions;
+
+namespace DiamondGameTests;
+
+public class DiamondGeneratorTests
+{
+	private DiamondGenerator sut;
+
+	[SetUp]
+	public void Setup()
+	{
+		sut = new DiamondGenerator();
+	}
+
+	[Test]
+	[TestCaseSource(nameof(GenerateAllDiamonds))]
+	public void When_DiamondGenerator_IsCalled_Proper_Diamond_ShouldBe_Returned((char FirstLetter, bool IncludeWhiteSpaces, string DiamondGeneratedAsString) diamondTestCase)
+	{
+		// Arrange
+		var expectedResult = diamondTestCase.DiamondGeneratedAsString;
+
+		// Act
+		var result = sut.GenerateDiamond(diamondTestCase.FirstLetter, diamondTestCase.IncludeWhiteSpaces);
+
+		// Assert
+		result.Should().NotBeNull();
+		result.Should().Be(expectedResult);
+	}
+
+	#region Diamond Test Case Data
+	private static IEnumerable<(char FirstLetter, bool IncludeWhiteSpaces, string Diamond)> GenerateAllDiamonds
+	{
+		get
+		{
+			yield return (FirstLetter: 'A', IncludeWhiteSpaces: false, Diamond: DiamondExamples.A_False);
+			yield return (FirstLetter: 'B', IncludeWhiteSpaces: false, Diamond: DiamondExamples.B_False);
+			yield return (FirstLetter: 'C', IncludeWhiteSpaces: false, Diamond: DiamondExamples.C_False);
+			yield return (FirstLetter: 'D', IncludeWhiteSpaces: false, Diamond: DiamondExamples.D_False);
+			yield return (FirstLetter: 'E', IncludeWhiteSpaces: false, Diamond: DiamondExamples.E_False);
+
+			yield return (FirstLetter: 'A', IncludeWhiteSpaces: true, Diamond: DiamondExamples.A_True);
+			yield return (FirstLetter: 'B', IncludeWhiteSpaces: true, Diamond: DiamondExamples.B_True);
+			yield return (FirstLetter: 'C', IncludeWhiteSpaces: true, Diamond: DiamondExamples.C_True);
+			yield return (FirstLetter: 'D', IncludeWhiteSpaces: true, Diamond: DiamondExamples.D_True);
+			yield return (FirstLetter: 'E', IncludeWhiteSpaces: true, Diamond: DiamondExamples.E_True);
+
+			//TO DO : fill remaining diamond test cases up to last letter Z
+		}
+	}
+
+	#region Diamond String Resources
+	private static class DiamondExamples
+	{
+		internal const string A_False = "A";
+		internal const string B_False =
+			@" A 
+B B
+ A ";
+		internal const string C_False =
+			@"  A  
+ B B 
+C   C
+ B B 
+  A  ";
+		internal const string D_False =
+			@"   A   
+  B B  
+ C   C 
+D     D
+ C   C 
+  B B  
+   A   ";
+		internal const string E_False =
+			@"    A    
+   B B   
+  C   C  
+ D     D 
+E       E
+ D     D 
+  C   C  
+   B B   
+    A    ";
+
+		internal const string A_True = "A";
+		internal const string B_True =
+			@"_ A _ 
+B _ B
+_ A _ ";
+		internal const string C_True =
+			@"_ _ A _ _ 
+_ B _ B _ 
+C _ _ _ C
+_ B _ B _ 
+_ _ A _ _ ";
+		internal const string D_True =
+			@"_ _ _ A _ _ _ 
+_ _ B _ B _ _ 
+_ C _ _ _ C _ 
+D _ _ _ _ _ D
+_ C _ _ _ C _ 
+_ _ B _ B _ _ 
+_ _ _ A _ _ _ ";
+		internal const string E_True =
+			@"_ _ _ _ A _ _ _ _ 
+_ _ _ B _ B _ _ _ 
+_ _ C _ _ _ C _ _ 
+_ D _ _ _ _ _ D _ 
+E _ _ _ _ _ _ _ E
+_ D _ _ _ _ _ D _ 
+_ _ C _ _ _ C _ _ 
+_ _ _ B _ B _ _ _ 
+_ _ _ _ A _ _ _ _ ";
+	}
+	#endregion
+
+	#endregion
+}
